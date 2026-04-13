e26ac3992ff2ba660bbf9022f69f8ac86464011a
deelanfu <142878595+deelanfu@users.noreply.github.com>
2026-04-13 20:43:03 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmigoFaceSwapSDK",
-            url: "https://downloads.amigoai.io/swiftpm/AmigoSDK_iOS/v1.0.0/AmigoFaceSwapSDK.xcframework.zip",
+            url: "https://downloads.amigoai.io/swiftpm/AmigoSDK_iOS/v1.0.1/AmigoFaceSwapSDK.xcframework.zip",
             checksum: "05cd9f74b8a8814af44ab5417ff6e800edbcc449352a0f4c6be1f9ed749c1236"
         )
     ]
